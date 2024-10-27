73e0fd8cbb229a0eb8c561eb43e0e4e8533c8303
Vishal Tayade <tayadevishal00@gmail.com>
2024-10-27 17:43:31 +0530

    increase and decrease product count

--- cartscript.js
+++ cartscript.js
@@ -32,6 +32,35 @@ function displayInCart(){
             card.children.item(0).children.item(2).children.item(1).children.item(0).children.item(0).innerText = obj.count;
             cartItems.appendChild(card);
             
+            // reduce item
+            card.children.item(0).children.item(2).children.item(0).children.item(0).addEventListener('click', () => {reduceProductCount(obj,card)});
+            // increase item
+            card.children.item(0).children.item(2).children.item(0).children.item(2).addEventListener('click', () => {increaseProductCount(obj,card)});
         }
     }
+}
+
+
+function reduceProductCount(obj,card){
+    obj.count--;
+    if(!obj.count) {
+        localStorage.removeItem(obj.id);
+        window.location.reload();
+        return
+    }
+    card.children.item(0).children.item(2).children.item(0).children.item(1).innerText = obj.count;
+    card.children.item(0).children.item(2).children.item(1).children.item(0).children.item(0).innerText = obj.count;
+    localStorage.setItem(obj.id, JSON.stringify(obj));
+    console.log(localStorage.getItem(obj.id), obj,card);
+    
+}
+
+
+function increaseProductCount(obj,card){
+    obj.count++;
+    card.children.item(0).children.item(2).children.item(0).children.item(1).innerText = obj.count;
+    card.children.item(0).children.item(2).children.item(1).children.item(0).children.item(0).innerText = obj.count;
+    localStorage.setItem(obj.id, JSON.stringify(obj));
+    console.log(localStorage.getItem(obj.id), obj,card);
+    
 }
